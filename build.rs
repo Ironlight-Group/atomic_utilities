a366aa76bce4af47d526e5c52c3cfcdf64e18a26
Adam Zellers <adam.zellers@ironlight.io>
2025-03-27 09:23:00 -0500

    Never set 'use_asm'

--- build.rs
+++ build.rs
@@ -1,11 +1,3 @@
 extern crate rustc_version;
-use rustc_version::{version_meta, Channel};
 
-fn main() {
-    match version_meta().channel {
-        Channel::Nightly => {
-            println!("cargo:rustc-cfg=use_asm");
-        },
-        _ => (),
-    }
-}
+fn main() {}
